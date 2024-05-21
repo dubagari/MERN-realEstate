4e50d01e48f8e60426567edb9e208089f742a643
Abubakar Dubagari <superango93@gmail.com>
2024-05-21 19:08:23 +0100

    create sign in api route

--- api/controller/auth.js
+++ api/controller/auth.js
@@ -1,6 +1,8 @@
 import User from "../model/usermodel.js";
 import { errorHandler } from "../util/error.js";
 import bcypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
 export const signUp = async (req, res, next) => {
   const { userName, email, password } = req.body;
   const hashPassword = bcypt.hashSync(password, 12);
@@ -11,5 +13,24 @@ export const signUp = async (req, res, next) => {
     res.status(201).json("User Created Successeful");
   } catch (error) {
     next(errorHandler(550, "duplicate key"));
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const valideUser = await User.findOne({ email });
+    if (!valideUser) return next(errorHandler(404, "user not found!"));
+    const validPassword = bcypt.compareSync(password, valideUser.password);
+    if (!validPassword) return next(errorHandler(401, "wrong credentails!"));
+
+    const token = jwt.sign({ id: valideUser._id }, process.env.JWT_SECRET);
+    const { password: pass, ...ress } = valideUser._doc;
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(ress);
+  } catch (error) {
+    next(error);
   }
 };
--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { signUp } from "../controller/auth.js";
+import { signUp, signin } from "../controller/auth.js";
 
 const router = express.Router();
 
 router.post("/signup", signUp);
+router.post("/signin", signin);
 
 export default router;
